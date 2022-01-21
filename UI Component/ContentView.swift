aa8b39137d24b9ac88580d375fdb96cfec03d322
Jian Zhang <jizhang@thoughtworks.com>
2022-01-21 13:49:51 +0800

    Initial Commit

---
+++ UI Component/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UI Component
+//
+//  Created by Jian on 2022/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UI Component/UI_ComponentApp.swift
@@ -0,0 +1,17 @@
+//
+//  UI_ComponentApp.swift
+//  UI Component
+//
+//  Created by Jian on 2022/1/21.
+//
+
+import SwiftUI
+
+@main
+struct UI_ComponentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
