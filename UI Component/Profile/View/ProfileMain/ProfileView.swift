1a58a7f1c025c08b77da4424d7c737ce77df6703
Monster1996 <280798304@qq.com>
2022-09-21 10:57:06 +0800

    0921 feat:add navigation for profileview

--- UI Component/Profile/View/ProfileMain/ProfileView.swift
+++ UI Component/Profile/View/ProfileMain/ProfileView.swift
@@ -9,34 +9,56 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-       Text("ProfilView")
+        NavigationView{
+            List(){
+                Section(){
+                    ProfileHeaderView()
+                }
+                Section(){
+                    NavigationLink(
+                        destination: {EmptyView()},
+                        label: {
+                            ListCell(imageName: "message.and.waveform", optionText: "服务")
+                        })
+                    NavigationLink(
+                        destination: {EmptyView()},
+                        label: {
+                            ListCell(imageName: "shippingbox",optionText: "收藏")
+                        })
+                    NavigationLink(
+                        destination: {EmptyView()},
+                        label: {
+                            ListCell(imageName: "menucard",optionText: "卡包")
+                        })
+                    NavigationLink(
+                        destination: {EmptyView()},
+                        label: {
+                            ListCell(imageName: "gearshape",optionText: "设置")
+                        })
+                }
             }
+            .navigationTitle("我的")
+            .navigationBarTitleDisplayMode(.inline)
         }
+    }
+}
+
+struct ListCell : View {
     
+    var imageName : String!
+    var optionText : String!
+    
+    var body: some View {
+        HStack{
+            Image(systemName: imageName)
+            Text(optionText)
+        }
+    }
+}
+
+
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
-        List(){
-            Section(){
-                ProfileHeaderView()
-            }
-            Section(){
-                HStack{
-                    Image(systemName: "message.and.waveform")
-                    Text("服务")
-                }
-                HStack{
-                    Image(systemName: "shippingbox")
-                    Text("收藏")
-                }
-                HStack{
-                    Image(systemName: "menucard")
-                    Text("卡包")
-                }
-                HStack{
-                    Image(systemName: "gearshape")
-                    Text("设置")
-                }
-            }
-        }
+        ProfileView()
     }
 }
