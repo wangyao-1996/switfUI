3fee8d84e513c4d3df97373fa4dae9f176667112
Monster1996 <280798304@qq.com>
2022-09-27 10:18:33 +0800

    refactor:code style

--- UI Component/Timeline/View/TimeLineHeaderView.swift
+++ UI Component/Timeline/View/TimeLineHeaderView.swift
@@ -11,12 +11,21 @@ struct TimelineHeaderView: View{
     var body: some View {
         ZStack(alignment: .bottomTrailing){
             
-            Image("timeline_profile_background").resizable().frame(width: UIScreen.main.bounds.width,height: 300).aspectRatio(contentMode: .fit).padding(.bottom)
+            Image("timeline_profile_background")
+                .resizable()
+                .frame(width: UIScreen.main.bounds.width,height: 300)
+                .aspectRatio(contentMode: .fit).padding(.bottom)
             
             HStack{
-                Text("桃子猪").font(.title).foregroundColor(.white)
-                Image("timeline_profile_image").resizable().frame(width: 100,height: 100).aspectRatio(contentMode:.fit)
-            }.padding(.trailing)
+                Text("桃子猪")
+                    .font(.title)
+                    .foregroundColor(.white)
+                Image("timeline_profile_image")
+                    .resizable()
+                    .frame(width: 100,height: 100)
+                    .aspectRatio(contentMode:.fit)
+            }
+            .padding(.trailing)
         }
     }
 }
