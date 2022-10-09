70d728f82943bdeaaacce0d5763837bbfc0a4fd9
Monster1996 <280798304@qq.com>
2022-10-09 10:31:56 +0800

    fix: add new file to move timeline content code

--- UI Component/Timeline/View/TimelineView.swift
+++ UI Component/Timeline/View/TimelineView.swift
@@ -32,54 +32,6 @@ struct TimelineView: View {
     }
 }
 
-struct TimeLineContent: View {
-    var profilePicture: String
-    var userName: String
-    var timeLineTextContent: String
-    var timeLineImageContent: String?
-    @State var likeArray: [String] = []
-    @State private var likeIcon: String = "heart"
-    @State private var isMyLike: Bool = false
-    
-    fileprivate func changeLikeLabelStyle() {
-        if isMyLike == false {
-            likeIcon = "heart.fill"
-            isMyLike = !isMyLike
-            likeArray.append("桃子猪")
-        } else {
-            likeIcon = "heart"
-            isMyLike = !isMyLike
-            likeArray.remove(at:(likeArray.firstIndex(of: "桃子猪"))!)
-        }
-    }
-    
-    var body: some View{
-        HStack(alignment:.top){
-            Image(profilePicture).resizable().frame(width: 50,height: 50).aspectRatio(contentMode:.fit)
-            VStack(alignment: .leading){
-                    Text(userName).font(.headline)
-                    Spacer()
-                    Text(timeLineTextContent)
-                    if timeLineImageContent != nil {
-                        Spacer()
-                        Image(timeLineImageContent!).resizable().aspectRatio(contentMode: .fit)
-                    }
-                    Spacer()
-                HStack(alignment: .top){
-                        Button(action:{
-                            changeLikeLabelStyle()
-                        }){
-                            Image(systemName: likeIcon).foregroundColor(.red)
-                        }
-                        Text(stringArrayToString(likeArray)).foregroundColor(.white)
-                        Spacer()
-                    }
-                    .background(likeArray.isEmpty ? Color.white : Color.gray)
-            }
-        }
-    }
-}
-
 struct TimelineView_Previews: PreviewProvider {
     static var previews: some View {
         TimelineView()
